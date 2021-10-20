e69220b811212077eba0d48e818629c137a57f4b
Will <w_birdseye@hotmail.co.uk>
2021-10-20 15:40:50 +0100

    challenge 10 complete making docking station get bikes

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -4,5 +4,9 @@ describe DockingStation do
   it "DockingStation object to respond to .release_bike" do
     expect(subject).to respond_to(:release_bike)
   end
+
+  it "bike is released" do
+    expect(subject.release_bike). to be_working
+  end 
 end
 
