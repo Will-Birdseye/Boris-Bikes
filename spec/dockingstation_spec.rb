e4c1f8eb2840f33c195ba58f1349d927f1b82b03
Will <w_birdseye@hotmail.co.uk>
2021-10-20 17:09:19 +0100

    challenge 11 complete bike dock now docks bikes

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -8,5 +8,24 @@ describe DockingStation do
   it "bike is released" do
     expect(subject.release_bike). to be_working
   end 
-end
 
+  it "responds to dock with 1 as an argument" do
+    expect(subject).to respond_to(:dock).with(1).argument
+  end
+
+  it "responds to bike method" do
+   expect(subject).to respond_to(:bike)
+  end
+
+  it "docks a bike" do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it "returns docked bikes" do
+   bike = Bike.new
+   subject.dock(bike)
+   expect(subject.bike).to eq bike
+  end
+
+end
